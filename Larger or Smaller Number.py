dec8ca471dba8e3a36265b9cd2d18b1c9d652ca6
Dhruv Kumar <71509305+the-multiverse-0-9@users.noreply.github.com>
2020-10-02 17:54:08 +0530

    Added Larger/Smaller Number Program
    Added Larger/Smaller Number Program.

---
+++ Larger or Smaller Number.py
@@ -0,0 +1,15 @@
+# Input two numbers and display the larger / smaller number
+a = int(input("Enter any number:"))
+b = int(input("Enter another number:"))
+if a>b :
+    print("The greater number is:", a)
+    print("The smaller number is:", b)
+elif a==b :
+    print("Both the numbers are equal")
+elif a<b :
+    print("The greater number is:", b)
+    print("The smaller number is:", a)
+else:
+    print("Try again")
+ 
+ 
