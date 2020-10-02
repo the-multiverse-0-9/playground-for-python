bcbb7478f2c39682fb0f1461c7ce7aebb74d0566
Dhruv Kumar <71509305+the-multiverse-0-9@users.noreply.github.com>
2020-10-02 17:39:43 +0530

    Added Addition Program
    Added Addition Program.

---
+++ addition.py
@@ -0,0 +1,5 @@
+# Addition of Two Numbers
+a = 5 # Variable 'a' is the first number
+b = 9 # Variable 'b' is the second number
+c = a + b # Variable 'c' contains the sum of above two numbers
+print(c) # This prints the result of addition
