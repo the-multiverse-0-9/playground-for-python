89224ee5f7e87e1ceeab01d47a11aca02ae50ea8
Dhruv Kumar <71509305+the-multiverse-0-9@users.noreply.github.com>
2020-10-06 17:21:37 +0530

    Created swapprogram.py
    Created a program to swap values of two variables.

---
+++ swapprogram.py
@@ -0,0 +1,7 @@
+# Program to swap values of two variables
+a,b = 7,5                               # a = 7 & b = 5
+print("The initial value of a is:", a)  # Prints the initial value of variable 'a'
+print("The initial value of b is:", b)  # Prints the initial value of variable 'b'  
+a,b = b,a                               # Swaps the values of 'a' & 'b'
+print("The final value of a is:", a)    # Prints the final value of variable 'a'
+print("The final value of b is:", b)    # Prints the final value of variable 'b'
